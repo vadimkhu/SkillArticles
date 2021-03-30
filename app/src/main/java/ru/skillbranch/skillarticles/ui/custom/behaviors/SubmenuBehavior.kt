f27d226b60c725e881749cb361a1ee729c3f05dd
vadimkhu <ccod@mail.ru>
2021-03-30 22:32:59 +0300

    Initial

--- app/src/main/java/ru/skillbranch/skillarticles/ui/custom/behaviors/SubmenuBehavior.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/ui/custom/behaviors/SubmenuBehavior.kt
@@ -1,4 +1,35 @@
 package ru.skillbranch.skillarticles.ui.custom.behaviors
 
-class SubmenuBehavior {
+import android.util.Log
+import android.view.View
+import androidx.coordinatorlayout.widget.CoordinatorLayout
+import androidx.core.view.marginRight
+import ru.skillbranch.skillarticles.ui.custom.ArticleSubmenu
+import ru.skillbranch.skillarticles.ui.custom.Bottombar
+
+class SubmenuBehavior : CoordinatorLayout.Behavior<ArticleSubmenu>() {
+    override fun layoutDependsOn(
+        parent: CoordinatorLayout,
+        child: ArticleSubmenu,
+        dependency: View
+    ): Boolean {
+        return dependency is Bottombar
+    }
+
+    override fun onDependentViewChanged(
+        parent: CoordinatorLayout,
+        child: ArticleSubmenu,
+        dependency: View
+    ): Boolean {
+        return if (child.isOpen && dependency is Bottombar && dependency.translationY >= 0) {
+            animate(child, dependency)
+            true
+        } else false
+    }
+
+    private fun animate(child: ArticleSubmenu, dependency: Bottombar) {
+        val fraction : Float = dependency.translationY / dependency.minHeight.toFloat()
+        child.translationX = (child.width + child.marginRight) * fraction
+        Log.e("SubmenuBehavior", "fraction : $fraction translationX : ${child.translationX}")
+    }
 }
